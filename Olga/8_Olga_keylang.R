ab771324f2bd2f13b0cab3a6ee3ff19fa4bd17ba
Teadmiseds <teadmiseds@gmail.com>
2017-05-10 19:36:26 +0200

    Added language

---
+++ Olga/8_Olga_keylang.R
@@ -0,0 +1,36 @@
+setwd("~/Desktop/Products3/key0405/en")
+#Create new "c"
+c <- data.frame()
+
+#Reed the files
+file_list <- list.files() 
+list_of_files <- lapply(file_list, read.csv)
+#Run bind
+for (i in 1:(length(file_list)))
+{
+  c <- rbind(c,list_of_files[[i]])
+}
+a<- matrix ("eng",nrow = length(c[,1]))
+a<- data.frame(Language=a)
+c <- cbind(c,a)
+
+
+setwd("~/Desktop/Products3/key0405/ger")
+#Create new "c"
+d <- data.frame()
+
+#Reed the files
+file_list <- list.files() 
+list_of_files <- lapply(file_list, read.csv)
+#Run bind
+for (i in 1:(length(file_list)))
+{
+  d <- rbind(d,list_of_files[[i]])
+}
+b<- matrix ("deu",nrow = length(d[,1]))
+b<- data.frame(Language=b)
+d <- cbind(d,b)
+fin <- rbind(c,d)
+colnames(fin)[18]<-"key"
+setwd("~/Desktop/Products3/key0405/")
+write.csv(fin,file="key_up_04_05_products.csv")
