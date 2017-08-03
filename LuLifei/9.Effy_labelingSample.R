90456db07445a425c2a6584d17b551817996ab43
Lu Lifei <lulifei1993@126.com>
2017-08-03 02:05:47 +0200

    Update and rename LuLifei_labelingSample.R to 9.Effy_labelingSample.R

--- LuLifei/9.Effy_labelingSample.R
+++ LuLifei/9.Effy_labelingSample.R
@@ -1,10 +1,17 @@
 
 
+
+# used for random selecting 800 tweets to do the labeling work
+
+
+
 #read in file
 df <- read.csv(file.choose(),header = TRUE,sep = ",")
 
+
 #save english tweets only
 df <- subset(df,df$Language=="eng")
+
 
 #random select 200 tweets of keywords "abbvie" for labeling
 abbvie <- subset(df,df$key=="abbvie")
@@ -12,11 +19,13 @@ rows <- nrow(abbvie)
 indexes <- sample(rows,200,replace = FALSE)
 abbvieSample <- abbvie[indexes,]
 
+
 #random seledt 200 tweets of keywords "adalimumab" for labeling
 adalimumab <- subset(df,df$key=="adalimumab")
 rows <- nrow(adalimumab)
 indexes <- sample(rows,200,replace = FALSE)
 adalimumabSample <- adalimumab[indexes,]
+
 
 #random seledt 200 tweets of keywords "amgen" for labeling
 amgen <- subset(df,df$key=="amgen")
@@ -24,11 +33,13 @@ rows <- nrow(amgen)
 indexes <- sample(rows,200,replace = FALSE)
 amgenSample <- amgen[indexes,]
 
+
 #random seledt 200 tweets of keywords "ankylosing" for labeling
 ankylosing <- subset(df,df$key=="ankylosing spondylitis")
 rows <- nrow(ankylosing)
 indexes <- sample(rows,200,replace = FALSE)
 ankylosingSample <- ankylosing[indexes,]
+
 
 #combine samples together and write as .csv file
 labelingSample <- rbind(abbvieSample,adalimumabSample,amgenSample,ankylosingSample)
@@ -36,4 +47,5 @@ label <- matrix(nrow = 800,ncol = 4)
 colnames(label) <- c("sentiment","sarcastic","context","topic")
 labelingSample <- cbind(labelingSample,label)
 write.csv(labelingSample,file = "LuLifei_labelingSample.csv")
+
 
