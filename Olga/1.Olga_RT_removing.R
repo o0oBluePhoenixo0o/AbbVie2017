ba0f72d92809aedbe146340a8b06fa25528bb0ac
Teadmiseds <teadmiseds@gmail.com>
2017-08-09 16:32:24 +0200

    Update 1.Olga_RT_removing.R

--- Olga/1.Olga_RT_removing.R
+++ Olga/1.Olga_RT_removing.R
@@ -1,3 +1,5 @@
+#How to delete retweets from file#
+#Loading the libraries
 library("readr")
 library("tm")
 library("NLP")
@@ -8,24 +10,4 @@ data <- read.csv("Products.csv", header = TRUE)
 data[1,]
 #Stripping
 data_new<-data[-grep("RT @",data$Text),]
-#Check 1 row
 data_new[1,]
-#Don't read after
-
-#In a loop
-#for (j in 1:(length(data$Text)-1)) {
-#  if(data$Text[j]==data$Text[j+1])
-#      {
-#      d2<-data[-grep("RT @",data$Text),]
-#  }
-#}
-
-#Delete a link
-#data<-gsub('http.* *', '', data$Text)
-#Cleaning
-#corpus <- Corpus(VectorSource(data_new))
-#corpus1 <-tm_map(corpus, tolower )
-#corpus1 <-tm_map(corpus, removePunctuation )
-#corpus1 <-tm_map(corpus1, stripWhitespace )
-#corpus1 <-tm_map(corpus1,stemDocument )
-#
